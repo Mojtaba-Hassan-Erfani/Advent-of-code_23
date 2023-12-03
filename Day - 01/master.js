6a7f1c4a626c63fc3299fcf37b708748a71a67af
Mojtaba Hassan ERFANI <erfani.mojtaba@hotmail.com>
2023-12-03 02:39:15 +0100

    [Day-1] Part 2

--- Day - 01/master.js
+++ Day - 01/master.js
@@ -1,31 +1,46 @@
 const fs = require('node:fs');
+const matches = {
+    'fiveight': '58', 'oneight': '18', 'threeight': '38', 'nineight': '98', 'eightwo': '82', 'twone': '21',
+    'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9',
+};
 
 fs.readFile( 'data.txt', 'utf8', ( err, data ) => {
     const lines = data.split( '\n' );
+    let tmp = '';
     let result = 0;
 
     lines.forEach( line => {
-        line = line.split('');
-        let tmp = '';
-        // Get the first number.
+        tmp = '';
+        line =getNumber( line );
+
+         // Get the first number.
         for ( char of line ) {
             if ( ! isNaN( parseInt( char ) ) ) {
-                tmp += String( char );
+                tmp += char;
                 break;
             }
         }
+
         // Get the last number.
-        for ( char of line.reverse() ) {
+        for ( char of line.split('').reverse() ) {
             if ( ! isNaN( parseInt( char ) ) ) {
-                tmp += String( char );
+                tmp += char;
                 break;
             }
         }
 
         result += parseInt( tmp );
     } );
+
     // Log the final result.
     console.log( result );
 } );
+
+const getNumber = ( line ) => {
+    for( let[ key, value ] of Object.entries( matches ) ) {
+        line = line.replaceAll( key, value );
+    }
+    return line;
+}
 
 
