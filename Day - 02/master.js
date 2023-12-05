eb228c7950a70c24b2d5bf5e2a94cc02d615efb0
Mojtaba Hassan ERFANI <erfani.mojtaba@hotmail.com>
2023-12-05 19:05:02 +0100

    [Day-2] Part two implementation

--- Day - 02/master.js
+++ Day - 02/master.js
@@ -1,34 +1,55 @@
 const fs = require( 'fs' );
 
 fs.readFile( 'data.txt', 'utf8', ( err, data ) => {
-    let id = 1;
     let result = 0;
     const lines = data.split( '\n' );
 
     for ( let line of lines ) {
-        let setIsOk = true;
-        line = line.split( ': ' ).at( -1 ).split( ';' );
+        let amount = 0;
+        let highest = { 'red': 0, 'green': 0, 'blue': 0 };
+
+        line = line.split( ': ' ).at( -1 );
+        line = line.split( '; ' );
 
         for ( let subset of line ) {
             subset = subset.split( ', ' );
 
             for ( let cube of subset ) {
                 cube = cube.trimStart().split( ' ' );
-                if ( cube[1].includes( 'red' ) && cube[0] > 12 ) {
-                    setIsOk = false;
-                    break;
-                } else if  ( cube[1].includes( 'green' ) && cube[0] > 13 ) {
-                    setIsOk = false;
-                    break;
-                } else if ( cube[1].includes( 'blue' ) && cube[0] > 14 ) {
-                    setIsOk = false;
-                    break;
+                if ( cube[1].includes( 'red' ) && cube[0] > parseInt( highest['red'] ) ) {
+                    highest['red'] = cube[0];
+                } else if ( cube[1].includes( 'green' ) && cube[0] > parseInt( highest['green'] ) ) {
+                    highest['green'] = cube[0];
+                } else if ( cube[1].includes( 'blue' ) && cube[0] > parseInt( highest['blue'] ) ) {
+                    highest['blue'] = cube[0];
                 }
             }
         }
 
-        if ( setIsOk ) result += id;
-        id++;
+        for ( const [cube, value] of Object.entries( highest ) ) {
+            if ( ! cube ) {
+                continue;
+            }
+
+            switch ( cube ) {
+                case 'red':
+                    if ( highest['green'] ) {
+                        amount += value * highest['green'];
+                    }
+                    if ( highest['blue'] && ! highest['green'] ) {
+                        amount += value * highest['blue'];
+                    }
+                    break;
+                case 'green':
+                    if ( highest['blue'] ) {
+                        amount = amount * highest['blue'];
+                    }
+                    break;
+            }
+        }
+
+        result += amount;
     }
+
     console.log( result );
 } )
