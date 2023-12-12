e2df7f626114a098428ff5669921738d70e623dc
Mojtaba Hassan ERFANI <erfani.mojtaba@hotmail.com>
2023-12-12 22:24:08 +0100

    [Day-3] Refactored version of part one

--- Day - 03/master.js
+++ Day - 03/master.js
@@ -13,48 +13,44 @@ fs.readFile( 'data.txt', 'utf8', ( err, data ) => {
         const lanes = [ lines[i-1], lines[i], lines[i+1] ];
 
         for ( let charIndex = 0; charIndex < line.length; ++charIndex ) {
-            // Check if there is a number when NaN charIndex is reached and handle the logic.
-            if ( isNaN( parseInt( line[charIndex] ) ) || charIndex == line.length - 1 ) {
-                if ( num ) {
-                    // If last char is a number add it to num.
-                    if(! isNaN( parseInt( line[charIndex] ) ) && charIndex == line.length - 1 ) {
-                        num += parseInt( line[charIndex] );
-                    }
-                    // Iterate from the line above to below, to check the symbol.
-                    for ( let lane of lanes ) {
-                        // Ignore the first and last lines in the file.
-                        if ( ! lane ) {
-                            continue;
-                        }
-
-                        lane = lane.split(''); // Need line as array again.
-
-                        // Get the correct startIndex incase the first char is a number.
-                        if( lane[charIndex - ( num.length + 1 )] ) {
-                            startIdx = charIndex - ( num.length + 1 );
-                        } else {
-                            startIdx = charIndex - num.length;
-                        }
-
-                        let foundNumber = false; // Flag to break the lane loop, if number is found.
-                        // Iterate in the range searching for symbol.
-                        for( let idx = startIdx; idx <= charIndex; ++idx ) {
-                            if( lane[idx] != '.' && isNaN( lane[idx] ) ) {
-                                result += parseInt( num );
-                                foundNumber = true;
-                                break;
-                            }
-                        }
-
-                        if( foundNumber ) break;
-                    }
-                    num = '';
-                }
-            } else {
-                // If charIndex is a number add it to num. ( for future, it's better to handle the information before the logic ).
+            const lastIndex = charIndex === line.length - 1;
+            const charIsDigit = ! isNaN( parseInt( line[charIndex] ) );
+            // While char is a number, add it to the num.
+            if ( charIsDigit ) {
                 num += String( line[charIndex] );
             }
+            // In case there is a number, check if it is legit. Also handle the last index in the line.
+            if ( ( num && ( ! charIsDigit || lastIndex ) )  ) {
+                // Iterate from the line above to below, to check the symbol.
+                for ( let lane of lanes ) {
+                    // Ignore the first and last lines in the file.
+                    if ( ! lane ) {
+                        continue;
+                    }
 
+                    lane = lane.split(''); // Need line as array again.
+
+                    // Get the correct startIndex in case the first char is a number.
+                    if( lane[charIndex - ( num.length + 1 )] ) {
+                        startIdx = charIndex - ( num.length + 1 );
+                    } else {
+                        startIdx = charIndex - num.length;
+                    }
+
+                    let foundNumber = false; // Flag to break the lane loop, if number is found.
+                    // Iterate in the possible range to search for symbol.
+                    for( let idx = startIdx; idx <= charIndex; ++idx ) {
+                        if( lane[idx] != '.' && isNaN( lane[idx] ) ) {
+                            result += parseInt( num );
+                            foundNumber = true;
+                            break;
+                        }
+                    }
+
+                    if( foundNumber ) break;
+                }
+                num = '';
+            }
         }
     }
     console.log( result );
